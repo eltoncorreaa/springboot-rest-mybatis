b50a9aefe7ae3bb00c97eddab83665f9a8903f9d
elton.correa <elton.correa@CPX-8MEO6AS7RN7.dir.svc.accenture.com>
2017-08-15 17:53:31 -0300

    incluindo testes unitarios com mockito e junit

---
+++ springboot-rest-ibatis/src/test/java/com/elton/app/service/impl/TaskServiceImplUnitTest.java
@@ -0,0 +1,37 @@
+package com.elton.app.service.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.elton.app.converter.TaskConverter;
+import com.elton.app.dto.TaskDTO;
+import com.elton.app.mapper.TaskMapper;
+import com.elton.app.model.Task;
+import com.elton.app.utils.factory.TaskMother;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TaskServiceImplUnitTest {
+
+	@InjectMocks
+	private TaskServiceImpl taskServiceImpl;
+
+	@Mock
+	private TaskMapper taskMapper;
+
+	@Test
+	public void persistTest() {
+		final TaskDTO taskDto = TaskMother.getTaskDTOPadrao();
+		final Task task = TaskMother.getTaskPadrao();
+		Mockito.when(taskMapper.insert(TaskConverter.toModel(taskDto))).thenReturn(1L);
+
+		Mockito.when(taskMapper.findById(1L)).thenReturn(task);
+		final TaskDTO retorno = taskServiceImpl.persist(taskDto);
+		Assert.assertEquals(TaskConverter.toModel(retorno), task);
+	}
+
+}
--- springboot-rest-ibatis/src/test/java/com/elton/app/utils/factory/TaskMother.java
+++ springboot-rest-ibatis/src/test/java/com/elton/app/utils/factory/TaskMother.java
@@ -1,10 +1,10 @@
 package com.elton.app.utils.factory;
 
-import java.util.Date;
-
+import org.joda.time.DateTime;
 import org.springframework.stereotype.Component;
 
 import com.elton.app.dto.TaskDTO;
+import com.elton.app.model.Task;
 
 /**
  * Classe utilizada nas classes de teste com a finalidade de criacao das
@@ -14,11 +14,23 @@ import com.elton.app.dto.TaskDTO;
 public class TaskMother {
 
 	private static final String NOME_TASK_TESTE_PADRAO = "Task Padrao";
+	private static final Integer ANO_2014 = 2014;
+
 
 	public static TaskDTO getTaskDTOPadrao() {
 		final TaskDTO taskDTO = new TaskDTO();
+		final DateTime dataInicio = new DateTime(ANO_2014, 1, 1, 0, 0, 0);
 		taskDTO.setName(NOME_TASK_TESTE_PADRAO);
-		taskDTO.setStartDate(new Date());
+		taskDTO.setStartDate(dataInicio.toDate());
 		return taskDTO;
+	}
+
+	public static Task getTaskPadrao() {
+		final Task task = new Task();
+		final DateTime dataInicio = new DateTime(ANO_2014, 1, 1, 0, 0, 0);
+		task.setId(1L);
+		task.setName(NOME_TASK_TESTE_PADRAO);
+		task.setStartDate(dataInicio.plusYears(1).toDate());
+		return task;
 	}
 }
