2fa1431dc872049e1f671088846282d6f2ee3844
eltonrufino <eltonrufino@hotmail.com>
2018-06-07 10:07:24 -0300

    Ajustando dependencia do mockito, ibatis e .gitignore

--- springboot-rest-ibatis/src/test/java/com/elton/app/service/impl/TaskServiceImplUnitTest.java
+++ springboot-rest-ibatis/src/test/java/com/elton/app/service/impl/TaskServiceImplUnitTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import com.elton.app.converter.TaskConverter;
 import com.elton.app.dto.TaskDTO;
